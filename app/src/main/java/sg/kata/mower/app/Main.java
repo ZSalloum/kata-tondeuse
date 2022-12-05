e3cbf2dea6e541ddf4b60745c1352d05577d52ae
zsalloum <ziad@solwai.com>
2022-12-05 18:17:14 +0100

    log main class

--- app/src/main/java/sg/kata/mower/app/Main.java
+++ app/src/main/java/sg/kata/mower/app/Main.java
@@ -6,11 +6,12 @@ import org.apache.logging.log4j.Logger;
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        logger.trace("Entering application.");
+        logger.info("Starting application.");
+
         String filepath = args[0];
         Starter starter = new Starter();
         starter.start(filepath);
 
-        System.out.println("-- end --");
+        logger.info("-- end --");
     }
 }
