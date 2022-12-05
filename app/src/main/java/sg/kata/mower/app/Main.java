5f4ec340323b95aa736bf9e851de1b24183d8854
zsalloum <ziad@solwai.com>
2022-12-05 18:14:11 +0100

    adjustments

--- app/src/main/java/sg/kata/mower/app/Main.java
+++ app/src/main/java/sg/kata/mower/app/Main.java
@@ -2,17 +2,6 @@ package sg.kata.mower.app;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import sg.kata.mower.app.analysis.ResultAnalysis;
-import sg.kata.mower.app.automation.CommandFactory;
-import sg.kata.mower.app.automation.Engine;
-import sg.kata.mower.app.models.Environment;
-import sg.kata.mower.app.parsers.SourceParser;
-import sg.kata.mower.app.readers.FileSource;
-import sg.kata.mower.core.analysis.IAnalysis;
-import sg.kata.mower.core.automation.ICommandFactory;
-import sg.kata.mower.core.models.IEnvironment;
-import sg.kata.mower.core.output.IDisplay;
-import sg.kata.mower.core.parsers.ISourceParser;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
